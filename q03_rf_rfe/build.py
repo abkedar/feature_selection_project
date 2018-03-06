47ac91f038f1de112a36369c393911e65056e080
abkedar <anupbhande@gmail.com>
2018-03-06 15:59:38 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -8,5 +8,13 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(df):
+    X = data.iloc[:, :-1]
+    y = data['SalePrice']
+    Rf = RandomForestClassifier()
+    rfe = RFE(Rf)
+    rfe1 = rfe.fit(X, y)
+    top_feat = X.columns[rfe.ranking_ == 1]
+    return top_feat.tolist()
 
-
+print rf_rfe(data)
