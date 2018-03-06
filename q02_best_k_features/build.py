059cde87da1ab783160a958ef907007c0bd3e027
abkedar <anupbhande@gmail.com>
2018-03-06 15:38:25 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -9,3 +9,13 @@ from sklearn.feature_selection import f_regression
 
 
 # Write your solution here:
+def percentile_k_features(df, k=20):
+    X=df.iloc[:,:-1]
+    y=df['SalePrice']
+    fs = SelectPercentile(f_regression, percentile=k)
+    fs1 = fs.fit_transform(X, y)
+    k1 = list(fs.scores_)
+    best_kindex = sorted(range(len(k1)), key=lambda i : k1[i], reverse=True)[:fs1.shape[1]]
+    best_k = [X.columns[i] for i in best_kindex]
+    return best_k
+print percentile_k_features(data, k=20)
