6392d890e2a5d46338ca8a108b26eacfbc1b0db5
abkedar <anupbhande@gmail.com>
2018-03-06 16:50:59 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -8,3 +8,16 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(df):
+
+    X = df.iloc[:, :-1]
+    y = df['SalePrice']
+    model = RandomForestClassifier()
+    model.fit(X, y)
+    b = SelectFromModel(model, prefit = True)
+
+    eleminate_k =X.columns[b.get_support() == True]
+    return eleminate_k.tolist()
+
+
+print select_from_model(data)
